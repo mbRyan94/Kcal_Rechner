adfbac84443ce51d7eb77da41d0c765cb9927e41
Mike Schertl <schert@hm.edu>
2018-02-08 16:53:43 +0100

    Added a bit more styling and the gender specific calculation

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ function calcKcal() {
   const size = document.getElementById('size').value;
   const age = document.getElementById('age').value;
   const weight = document.getElementById('weight').value;
-  document.getElementById('kcal').innerHTML = Math.floor(66.47 +(13.7 * weight) + (5 * size) - (6.8 * age));
-};
-
- //calcKcal(size, age, weight);
+/*
+  if (!Number.isInteger(size) || size < 0) {
+    document.getElementById('size').value = prompt("Bitte trage eine gültige Köpergröße in cm an.");
+  }
+*/
+  if (document.getElementById('gender_female').checked == true) {
+    document.getElementById('kcal').innerHTML ="Dein Grundumsatz liegt bei: " + Math.floor(655.1 +(9.6 * weight) + (1.8 * size) - (4.7 * age)) + " Kcal";
+  } else {
+    document.getElementById('kcal').innerHTML ="Dein Grundumsatz liegt bei: " + Math.floor(66.47 +(13.7 * weight) + (5 * size) - (6.8 * age)) + " Kcal";
+  }
+}
